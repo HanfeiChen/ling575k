718a3647d8ab44f7fe38918ab2258c7d8d99d668
David Chen <hanfec@cs.washington.edu>
2021-04-19 11:22:24 -0700

    add scripts for condor

---
+++ hw3/hw3.cmd
@@ -0,0 +1,4 @@
+executable = run_hw3.sh
+output     = hw3.out
+error      = hw3.error
+queue
---
+++ hw3/run_hw3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate /dropbox/20-21/575k/env
+
+# put your command for running word2vec.py here
+python word2vec.py \
+    --num_epochs 15 \
+    --save_vectors vectors.tsv \
+    --embedding_dim 15 \
+    --learning_rate 0.2 \
+    --min_freq 5 \
+    --num_negatives 15 \
+    --training_data /dropbox/20-21/575k/data/sst/train-reviews.txt
