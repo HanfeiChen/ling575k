5f3dcbfd33906d0f88b5332f0a821021b329324c
David Chen <hanfec@cs.washington.edu>
2021-04-26 23:03:42 -0700

    update run script

--- hw4/run_hw4.sh
+++ hw4/run_hw4.sh
@@ -4,6 +4,6 @@ conda activate /dropbox/20-21/575k/env
 
 python run.py
 
-# python run.py --l2 0.00001
+python run.py --l2 0.00001
 
-# python run.py --l2 0.00001 --word_dropout 0.3
+python run.py --l2 0.00001 --word_dropout 0.3
